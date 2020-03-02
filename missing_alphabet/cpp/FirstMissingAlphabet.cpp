33470e027bedef0a82c9ef65afb0ffbba5ed9f4d
anil kanduri <spakan2@gmail.com>
2020-03-02 12:10:40 -0800

    missing alphabet solution

--- missing_alphabet/cpp/FirstMissingAlphabet.cpp
+++ missing_alphabet/cpp/FirstMissingAlphabet.cpp
@@ -1,17 +1,39 @@
 #include <gtest/gtest.h>
 #include <string.h>
 
+#define NUM_CHAR 26
+
 char FirstMissingAlphabet(std::string inputString)
 {
     // Find first missing alphabet
-    //test comment
-    return 0;
+    char missing;
+    bool foundList[NUM_CHAR] = {false}; //running list for marking found alphabets
+    /*
+    Scan the input string and mark the found alphabets.
+    Check the found list until missing alphabet is reached.
+    Treat upper and lower case as same, ignore special charaters (?!)
+    */
+    for(auto i = 0; i < inputString.size(); i++){
+      if(inputString[i] >= 'a' && inputString[i] <= 'z'){
+        foundList[inputString[i] - 'a'] = true;
+      }else if(inputString[i] >= 'A' && inputString[i] <= 'Z'){
+        foundList[inputString[i] - 'A'] = true;
+      }
+    }
+    bool found = false;
+    for(auto i = 0; i < NUM_CHAR && !found; i++){
+      if(!foundList[i]){
+        missing = 'a' + i;
+        found = true;
+      }
+    }
+    return missing;
 }
 
 TEST(FirstMissingAlphabet, OneCharacterInputMissingA)
 {
-    const std::string inputString = "b";
-    const char expectedMissing = 'a';
+    const std::string inputString = "abbZXwE3!&/AWbbdd";
+    const char expectedMissing = 'c';
     char foundMissing = FirstMissingAlphabet(inputString);
     ASSERT_EQ(expectedMissing, foundMissing);
 }
